eade504a4672bff25753890d8b27b6c933ca5282
altaeva_al <altaeva_al@vk.com>
2025-02-06 14:17:35 +0300

    lesson #3

---
+++ app/src/androidTest/java/org/wikipedia/homeworks/homework03/OnboardingScreen.kt
@@ -0,0 +1,28 @@
+package org.wikipedia.homeworks.homework03
+
+import com.google.android.material.button.MaterialButton
+
+val skipButton = listOf(
+    MaterialButton::class.java,
+    "fragment_onboarding_skip_button",
+    "onboarding_skip"
+)
+
+val continueButton = listOf(
+    MaterialButton::class.java,
+    "fragment_onboarding_forward_button",
+    "onboarding_continue"
+)
+
+val addOrEditLanguages = listOf(
+    MaterialButton:: class.java,
+    "addLanguageButton",
+    "onboarding_multilingual_add_language_text"
+)
+
+
+
+
+
+
+
